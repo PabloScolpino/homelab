6e2fd2e8cab8d17550d92f1358bcc954cc182048
Pablo Scolpino <pabloscolpino@gmail.com>
2022-04-04 19:34:06 -0300

    parametarising disk name

--- 1_cloud_image/3_create_qemu_template.sh
+++ 1_cloud_image/3_create_qemu_template.sh
@@ -5,25 +5,32 @@
 #
 #
 #
+disk_name=local-lvm
 
 # 1. Create template VM
-qm create 9000 --memory 2048 --cores 2 --net0 virtio,bridge=vmbr0
+qm create 9000 \
+  --bios ovmf \
+  --memory 2048 \
+  --cores 2 \
+  --net0 virtio,bridge=vmbr0 \
+  --ostype l26
+
 
 # 2. Import Pristine image downloaded from canonicle
-qm importdisk 9000 focal-server-cloudimg-amd64.img.orig local-lvm
+qm importdisk 9000 focal-server-cloudimg-amd64.img.orig ${disk_name}
 
 
 # 2.b These were other attempts that did not quite work
 
 # 2.b.1 Personalized focal server image with virt-customize
-# qm importdisk 9000 focal-server-cloudimg-amd64.img local-lvm
+# qm importdisk 9000 focal-server-cloudimg-amd64.img ${disk_name}
 
 # 2.b.2 Pristine debian cloud-image
-# qm importdisk 9000 debian-11-genericcloud-amd64-20220310-944.qcow2 local-lvm
+# qm importdisk 9000 debian-11-genericcloud-amd64-20220310-944.qcow2 ${disk_name}
 
 # 3. Configure the VM
-qm set 9000 --scsihw virtio-scsi-pci --scsi0 local-lvm:vm-9000-disk-0
-qm set 9000 --ide2 local-lvm:cloudinit
+qm set 9000 --scsihw virtio-scsi-pci --scsi0 ${disk_name}:vm-9000-disk-0
+qm set 9000 --ide2 ${disk_name}:cloudinit
 qm set 9000 --boot c --bootdisk scsi0
 qm set 9000 --serial0 socket --vga serial0
 qm set 9000 --agent enabled=1
@@ -49,4 +56,3 @@ qm set 9000 --agent enabled=1
 
 # 6. set vm as template
 # qm template 9000
-
