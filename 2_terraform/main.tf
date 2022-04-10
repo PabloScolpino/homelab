f5e903a1968520dc5a032fc7084cc19180f143d4
Pablo Scolpino <pabloscolpino@gmail.com>
2022-04-09 22:20:39 -0300

    fixes for terraform vm config

--- 2_terraform/main.tf
+++ 2_terraform/main.tf
@@ -1,68 +1,65 @@
-resource "proxmox_vm_qemu" "pihole_vm" {
-  target_node = var.target_node
-  name        = "pihole.ar.olumpos.local"
-  desc        = "Pi-Hole VM"
-
-  os_type = "cloud-init"
-  clone   = var.template_name
-  count   = 1
-  vmid    = 100 + var.pihole_id
-
-  onboot = true
-  boot   = "c"
-  agent  = 1
-
-  bios    = "ovmf"
-  sockets = 1
-  cores   = 2
-  numa    = true
-  balloon = 768
-  memory  = 3072
-
-  disk {
-    size    = "29900M"
-    type    = "scsi"
-    storage = var.disk_storage
-    ssd     = 1
-  }
-
-  network {
-    model  = "virtio"
-    bridge = "vmbr0"
-  }
-
-  lifecycle {
-    # prevent_destroy = true
-    ignore_changes = [
-      # network, vmid, clone, full_clone, qemu_os
-    ]
-  }
-
-  ciuser       = var.ciuser
-  sshkeys      = var.sshkeys
-  ipconfig0    = "ip=10.0.0.${var.pihole_id}/24,gw=${var.gateway}"
-  nameserver   = var.nameserver
-  searchdomain = var.searchdomain
-}
+#resource "proxmox_vm_qemu" "pihole_vm" {
+#  target_node = var.target_node
+#  name        = "pihole.ar.olumpos.local"
+#  desc        = "Pi-Hole VM"
+#
+#  os_type = "cloud-init"
+#  clone   = var.template_name
+#  count   = 1
+#  vmid    = 100 + var.pihole_id
+#
+#  onboot = true
+#  boot   = "c"
+#  agent  = 1
+#
+#  bios    = "ovmf"
+#  sockets = 1
+#  cores   = 2
+#  numa    = true
+#  balloon = 768
+#  memory  = 3072
+#
+#  disk {
+#    size    = "29900M"
+#    type    = "scsi"
+#    storage = var.disk_storage
+#    ssd     = 1
+#  }
+#
+#  network {
+#    model  = "virtio"
+#    bridge = "vmbr0"
+#  }
+#
+#  lifecycle {
+#    # prevent_destroy = true
+#    ignore_changes = [
+#      # network, vmid, clone, full_clone, qemu_os
+#    ]
+#  }
+#
+#  ciuser       = var.ciuser
+#  sshkeys      = var.sshkeys
+#  ipconfig0    = "ip=10.0.0.${var.pihole_id}/24,gw=${var.gateway}"
+#  nameserver   = var.nameserver
+#  searchdomain = var.searchdomain
+#}
 
 resource "proxmox_vm_qemu" "unifi_vm" {
   target_node = var.target_node
+  clone       = var.template_name
+  os_type     = "cloud-init"
+
+  vmid        = 120
   name        = "unifi.ar.olumpos.local"
   desc        = "Unifi controller VM"
-
-  os_type = "cloud-init"
-  clone   = var.template_name
-  count   = 1
-  vmid    = 100 + var.unifi_id
 
   onboot = true
   boot   = "c"
   agent  = 1
-
   bios    = "ovmf"
-  sockets = 1
+
   cores   = 2
-  numa    = true
   balloon = 512
   memory  = 3072
 
@@ -78,43 +75,79 @@ resource "proxmox_vm_qemu" "unifi_vm" {
     bridge = "vmbr0"
   }
 
-  lifecycle {
-    # prevent_destroy = true
-    ignore_changes = [
-      # network, vmid, clone, full_clone, qemu_os
-    ]
-  }
-
   ciuser       = var.ciuser
   sshkeys      = var.sshkeys
-  ipconfig0    = "ip=10.0.0.${var.unifi_id}/24,gw=${var.gateway}"
+  ipconfig0    = "ip=10.0.0.20/24,gw=${var.gateway}"
   nameserver   = var.nameserver
   searchdomain = var.searchdomain
 }
 
-resource "proxmox_vm_qemu" "k8s-node" {
-  target_node = var.target_node
-  name        = "k8s-${count.index + 1}.ar.olumpos.local"
-  desc        = "Container cluster number ${count.index + 1}"
+#resource "proxmox_vm_qemu" "k8s-node" {
+#  target_node = var.target_node
+#  name        = "k8s-${count.index + 1}.ar.olumpos.local"
+#  desc        = "Container cluster number ${count.index + 1}"
+#
+#  os_type = "cloud-init"
+#  clone   = var.template_name
+#  count   = 1
+#  vmid    = 100 + var.k8s_node_id + count.index
+#
+#  onboot = true
+#  boot   = "c"
+#  agent  = 1
+#
+#  bios    = "ovmf"
+#  sockets = 1
+#  cores   = 4
+#  numa    = true
+#  balloon = 4096
+#  memory  = 16384
+#
+#  disk {
+#    size    = "203980M"
+#    type    = "scsi"
+#    storage = var.disk_storage
+#    ssd     = 1
+#  }
+#
+#  network {
+#    model  = "virtio"
+#    bridge = "vmbr0"
+#  }
+#
+#  lifecycle {
+#    ignore_changes = [
+#      network,
+#    ]
+#  }
+#
+#  ciuser       = var.ciuser
+#  sshkeys      = var.sshkeys
+#  ipconfig0    = "ip=10.0.0.${var.k8s_node_id + count.index}/24,gw=${var.gateway}"
+#  nameserver   = var.nameserver
+#  searchdomain = var.searchdomain
+#}
 
-  os_type = "cloud-init"
-  clone   = var.template_name
-  count   = 1
-  vmid    = 100 + var.k8s_node_id + count.index
+resource "proxmox_vm_qemu" "plex" {
+  target_node = var.target_node
+  clone       = var.template_name
+  os_type     = "cloud-init"
+
+  vmid        = 160
+  name        = "plex.ar.olumpos.local"
+  desc        = "Plex VM"
 
   onboot = true
   boot   = "c"
   agent  = 1
-
   bios    = "ovmf"
-  sockets = 1
-  cores   = 4
-  numa    = true
-  balloon = 4096
-  memory  = 16384
+
+  cores   = 2
+  balloon = 2048
+  memory  = 8192
 
   disk {
-    size    = "203980M"
+    size    = "29900M"
     type    = "scsi"
     storage = var.disk_storage
     ssd     = 1
@@ -125,15 +158,9 @@ resource "proxmox_vm_qemu" "k8s-node" {
     bridge = "vmbr0"
   }
 
-  lifecycle {
-    ignore_changes = [
-      network,
-    ]
-  }
-
   ciuser       = var.ciuser
   sshkeys      = var.sshkeys
-  ipconfig0    = "ip=10.0.0.${var.k8s_node_id + count.index}/24,gw=${var.gateway}"
+  ipconfig0    = "ip=10.0.0.60/24,gw=${var.gateway}"
   nameserver   = var.nameserver
   searchdomain = var.searchdomain
 }
