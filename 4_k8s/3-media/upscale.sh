db83db5fd46db8bc427409213beb3ee3e5125a70
Pablo Scolpino <pabloscolpino@gmail.com>
2022-05-25 21:35:42 +0200

    Tweak pv pvc for bazar

---
+++ 4_k8s/3-media/upscale.sh
@@ -0,0 +1,4 @@
+count=1
+kubectl scale --replicas=$count deployment.apps/deluge -n media
+kubectl scale --replicas=$count deployment.apps/radar -n media
+kubectl scale --replicas=$count deployment.apps/sonar -n media
