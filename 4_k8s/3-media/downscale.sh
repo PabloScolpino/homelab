5b1a19e25addc9b90eb8fecfb541668240910abb
Pablo Scolpino <pabloscolpino@gmail.com>
2022-09-21 00:11:29 +0200

    Rename nfs shares

--- 4_k8s/3-media/downscale.sh
+++ 4_k8s/3-media/downscale.sh
@@ -2,3 +2,4 @@ count=0
 kubectl scale --replicas=$count deployment.apps/deluge -n media
 kubectl scale --replicas=$count deployment.apps/radar -n media
 kubectl scale --replicas=$count deployment.apps/sonar -n media
+kubectl scale --replicas=$count deployment.apps/bazar -n media
