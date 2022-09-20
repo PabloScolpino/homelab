fdaafd9d120e783126ff45bc2aaa5be0940fd7aa
Pablo Scolpino <pabloscolpino@gmail.com>
2022-09-20 23:08:42 +0200

    Fede & Pablo VMs for work

--- 2_terraform/main.tf
+++ 2_terraform/main.tf
@@ -176,7 +176,7 @@ resource "proxmox_vm_qemu" "minecraft" {
   memory  = 8192
 
   disk {
-    size    = "40000M"
+    size    = "39116M"
     type    = "scsi"
     storage = var.disk_storage
     ssd     = 1
@@ -188,4 +188,82 @@ resource "proxmox_vm_qemu" "minecraft" {
   }
 
   ipconfig0    = "ip=10.0.0.61/24,gw=${var.gateway}"
+}
+
+resource "proxmox_vm_qemu" "workstation-pablo" {
+  target_node = var.target_node
+  clone       = var.template_name
+  os_type     = "cloud-init"
+
+  onboot = true
+  boot   = "c"
+  agent  = 1
+  bios    = "ovmf"
+
+  ciuser       = var.ciuser
+  sshkeys      = var.sshkeys
+  nameserver   = var.nameserver
+  searchdomain = var.searchdomain
+
+  vmid        = 162
+  name        = "work.ar.olumpos.lan"
+  desc        = "Workstation"
+
+  cpu     = "host,hidden=1"
+  cores   = 4
+  balloon = 4096
+  memory  = 8192
+
+  disk {
+    size    = "99532M"
+    type    = "scsi"
+    storage = var.disk_storage
+    ssd     = 1
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  ipconfig0    = "ip=10.0.0.62/24,gw=${var.gateway}"
+}
+
+resource "proxmox_vm_qemu" "workstation-fede" {
+  target_node = var.target_node
+  clone       = var.template_name
+  os_type     = "cloud-init"
+
+  onboot = true
+  boot   = "c"
+  agent  = 1
+  bios    = "ovmf"
+
+  ciuser       = "fede"
+  sshkeys      = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/Jkpw0NJxXC0sgPMxIVhjt9rSvkGeLskGBzuHX0OefZwQmM4cB9ZoL8fg7W5ZXwY9e9dbisD4YysRJMwJEZ0X+E2JYv68UZ3Q7WT9ASWgbLrhG/YQvBDE9vsfuvSBhtuDsRyoSLYgXITG3aQqm1+9TVk1KbDF0RJ/GWkeAnp8dftmJKrwAeZjd0les/r/e39EG53ikdZzTaJS+JAE5h415DUfYd9V0fkwDVcPxs+a04HWz09wN2H4qs8JpkXNSBSlONUQrAbOzrk2NvoLe9yNxFdg64SJnf5dxSwRoz3wbmFWxcHpFMyqgR8SGAYtxxEaAHH+Q3o3X86jO3m4NLNt fedex@azula"
+  nameserver   = var.nameserver
+  searchdomain = var.searchdomain
+
+  vmid        = 163
+  name        = "work-fede.ar.olumpos.lan"
+  desc        = "Workstation for Fede"
+
+  cpu     = "host,hidden=1"
+  cores   = 4
+  balloon = 4096
+  memory  = 8192
+
+  disk {
+    size    = "99532M"
+    type    = "scsi"
+    storage = var.disk_storage
+    ssd     = 1
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  ipconfig0    = "ip=10.0.0.63/24,gw=${var.gateway}"
 }
