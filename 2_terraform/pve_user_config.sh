b306b6ba6f5ce5e7f3dfedafdf51b7f2f3208e3c
Pablo Scolpino <pabloscolpino@gmail.com>
2023-05-12 20:54:27 -0300

    Encrypting more files

--- 2_terraform/pve_user_config.sh
+++ 2_terraform/pve_user_config.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-
-
-
 echo "Input password for terraform-prov:"
 read -s password
 echo "again for confirmation:"
@@ -13,7 +10,6 @@ then
   echo "Passwords do not match"
   exit 1
 fi
-
 
 pveum role add TerraformProv -privs "VM.Allocate VM.Clone VM.Config.CDROM VM.Config.CPU VM.Config.Cloudinit VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Monitor VM.Audit VM.PowerMgmt Datastore.AllocateSpace Datastore.Audit"
 pveum user add terraform-prov@pve --password ${password}
