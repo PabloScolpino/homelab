48d791e63c4707e75be92491c9c6516eb1a09255
Pablo Scolpino <pabloscolpino@gmail.com>
2023-09-12 20:30:45 -0300

    Refactor util scripts for k8s

--- 4_k8s/3-media/create.sh
+++ 4_k8s/3-media/create.sh
@@ -1,3 +1,5 @@
 kubectl apply -f 1-persistent-volume-ar.yml
 kubectl apply -f 3-persistent-volume-claim.yml
-kubectl apply -f 8-bazar.yml
+
+service=$1
+kubectl scale --replicas=1 deployment.apps/$service -n media
--- 4_k8s/3-media/scale.sh
+++ 4_k8s/3-media/scale.sh
@@ -1,5 +1,3 @@
-count=1
-kubectl scale --replicas=$count deployment.apps/deluge -n media
-kubectl scale --replicas=$count deployment.apps/radar -n media
-kubectl scale --replicas=$count deployment.apps/sonar -n media
-kubectl scale --replicas=$count deployment.apps/bazar -n media
+service=$1
+count=$2
+kubectl scale --replicas=$count deployment.apps/$service -n media
