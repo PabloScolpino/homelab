da6298ee8b6facef4384666d1dbeacaf43fb80da
Pablo Scolpino <pabloscolpino@gmail.com>
2025-03-30 16:47:33 -0300

    refactor base makefile

--- 4_k8s/common.mk
+++ 4_k8s/common.mk
@@ -2,28 +2,12 @@
 all: help
 
 help:
-	@awk 'BEGIN {FS = ":.*?## "; printf "\033[36m%-30s\033[0m %s\n", "Target", "Description"} /^[a-zA-Z0-9_-]+:.*?## / {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST) | sort
+	@awk 'BEGIN {FS = ":.*?## "; printf "\033[36m%-30s\033[0m %s\n", "Action", "Description"} /^[a-zA-Z0-9_-]+:.*?## / {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST) | sort
 
 K:=kubectl
 APPLY:=$(K) apply -f
 DELETE:=$(K) delete -f
 
-################################################################################
-### K8s Actions
-create_namespace: ## Create the namespace if missing
-	@echo
-	# Creating Namespace
-	-$(K) create namespace $(NS)
-
-create_pv: ## Create the persistent volumes
-	@echo
-	# Creating PV
-	$(APPLY) 1-persistent-volume.yml
-
-create_pvc: ## Create the persistent volumes claims
-	@echo
-	# Creating PVC
-	$(APPLY) 2-persistent-volume-claim.yml
 
 list_volumes: ## Show volumes
 	@echo
@@ -31,99 +15,9 @@ list_volumes: ## Show volumes
 	$(K) get pv
 	
 	@echo
-	# PersistentVolumeClaims for namespace
-	$(K) get pvc --namespace $(NS)
-
-delete_volumes: ## Show volumes
-	@echo
-	# Deleting PV
-	$(DELETE) 1-persistent-volume.yml
-
-	@echo
-	# Deleting PVC
-	$(DELETE) 2-persistent-volume-claim.yml
-
-create_backup: ## Create the backup schedule
-	@echo
-	# Creating the backup
-	$(APPLY) 3-backup.yml
-
-create_secret_backup: ## Create the secret for backup
-	-$(K) create secret generic backup \
-		--from-literal=applicationKey="changeme" \
-		--from-literal=keyID="changeme" \
-		--from-literal=password="changeme" \
-		--namespace=vehicle
-
-	$(K) modify-secret backup --namespace vehicle
-
-create_deployment: ## Create the deployment
-	@echo
-	# Creating the deployment
-	$(APPLY) 4-deployment.yml
-
-create_deployments: ## Create the services' deployments
-	for service in $(SERVICES); do \
-		echo ""; \
-		echo "Creating deployment for $$service"; \
-		$(APPLY) 4-$$service.yml --namespace $(NS); \
-	done
-
-create_ingress: ## Create the ingress
-	@echo
-	# Creating the ingress route
-	$(APPLY) 5-ingress.yml
-
-up: ## Scale deployment to 1
-	@echo
-	# STARTING all services
-	for service in $(SERVICES); do \
-		$(K) scale --replicas=1 deployment.apps/$$service --namespace $(NS); \
-	done
-
-down: ## Scale deployment to 0
-	@echo
-	# STOPPING all services
-	for service in $(SERVICES); do \
-		$(K) scale --replicas=0 deployment.apps/$$service --namespace $(NS); \
-	done
-
-################################################################################
-### Helm Actions
-install_chart:
-	helm repo add $(CHART_REPO)
-	helm repo update
-
-check_helm_secrets: ## Check if helm secrets file secrets.yml exists
-	@if [ ! -f secrets.yml ]; then \
-		echo "Error: secrets.yml not found. Please create it from the template:"; \
-		echo "cp secrets.yml.template secrets.yml"; \
-		echo "Then edit secrets.yml with your actual secrets."; \
-		exit 1; \
-	fi
-
-create_helm_secrets: ## Create the helm secrets file
-	cp secrets.yml.template secrets.yml
-	vi secrets.yml
-
-install_application: install_chart
-	@HELM_CMD="helm install $(APP) $(CHART) --namespace $(NS)";\
-	if [ -f values.yml ]; then \
-		HELM_CMD="$$HELM_CMD -f values.yml"; \
-	fi; \
-	if [ -f secrets.yml ]; then \
-		$$HELM_CMD -f secrets.yml; \
+	# PersistentVolumeClaim
+	@if [ -z "$(NS)" ]; then \
+		$(K) get pvc --all-namespaces; \
 	else \
-		$$HELM_CMD; \
-	fi
-
-update_application:
-	@HELM_CMD="helm upgrade $(APP) $(CHART) --namespace $(NS)";\
-	if [ -f values.yml ]; then \
-		HELM_CMD="$$HELM_CMD -f values.yml"; \
-	fi; \
-	if [ -f secrets.yml ]; then \
-		$$HELM_CMD -f secrets.yml; \
-	else \
-		$$HELM_CMD; \
+		$(K) get pvc --namespace $(NS); \
 	fi
